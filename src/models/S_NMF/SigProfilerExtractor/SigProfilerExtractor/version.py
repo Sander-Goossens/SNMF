b8ae92ca49ed107c26da3a3266019866fa984590
Sander-Goossens <sander.goossens2@gmail.com>
2024-01-03 16:37:00 +0100

    clean code

--- src/models/S_NMF/SigProfilerExtractor/SigProfilerExtractor/version.py
+++
@@ -1,7 +0,0 @@
-
-# THIS FILE IS GENERATED FROM SIGPROFILEREXTRACTOR SETUP.PY
-short_version = '1.1.4'
-version = '1.1.4'
-Update = 'Heirarchy option deleted, clustering deleted and signatures orders by the mutation burden'
-    
-    
